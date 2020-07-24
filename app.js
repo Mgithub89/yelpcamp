9ff26c3091980d452b6cc1c3abc96efaea5b75ae
root <metages09@gmail.comgit config --global user.name metagesgit config --global user.email metages09@gmail.com>
2020-07-24 05:46:53 +0000

    add app.js

--- app.js
+++ app.js
@@ -59,7 +59,7 @@ app.use("/campgrounds",campgroundRoutes);
 app.use("/campgrounds/:id/comments",commentRoutes);
 
 
-app.listen(process.env.PORT, process.env.IP ||80, function(){
+app.listen(process.env.PORT, process.env.IP , function(){
    console.log("Server is listening!!!"); 
 });
 
