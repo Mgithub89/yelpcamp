43ce3568a724698e3c045e4f95d405186fe52608
root <metages09@gmail.comgit config --global user.name metagesgit config --global user.email metages09@gmail.com>
2020-08-01 20:21:46 +0000

    update app.js

--- app.js
+++ app.js
@@ -18,29 +18,16 @@ var commentRoutes = require("./routes/comments"),
 	authRoutes = require("./routes/auth");
 var url = process.env.DATABASEURL || 'mongodb://localhost:27017/yelp_camp'
 
-mongoose.connect(url,{
-useNewUrlParser: true,
-    useUnifiedTopology: true
-});
+mongoose.connect(url,{useNewUrlParser: true, useUnifiedTopology: true});
 
-// mongoose.connect(process.env.DATABASEURL,{
-// 	useNewUrlParser: true,
-//     useUnifiedTopology: true
-// });
+// mongoose.connect(process.env.DATABASEURL,{ useNewUrlParser: true,  useUnifiedTopology: true });
 
-// mongoose.connect('mongodb://localhost:27017/yelp_camp', {
-//   useNewUrlParser: true,
-//   useUnifiedTopology: true
-// });
-
-// mongoose.connect("mongodb+srv://Mongo0415:Mongodb89@cluster0.jf0aj.mongodb.net/yelpcamp?retryWrites=true&w=majority",{
+// mongoose.connect("mongodb+srv://<userid>:<password>@cluster0.jf0aj.mongodb.net/yelpcamp?retryWrites=true&w=majority",{
 // useNewUrlParser: true,
 // useUnifiedTopology: true	
 // }).then(()=>{
 // console.log("connected to DB");
 // }).catch(err=>{console.log("ERROR",err.message)});
-
-
 
 app.use(bodyparser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
